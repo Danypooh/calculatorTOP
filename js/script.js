fa4d1868ebb254071ecc89d5e65619e2bda51093
Daniel <delatorrechapell@gmail.com>
2022-10-10 13:52:16 -0500

    -handleError function:     checks if an operator was clicked first and handles the error
    -rounds result to 3 decimals

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ function toDisplay(button) {  //adds the value of the button to the display
         displayed = display.value;
     }
     else if (button.value == "=") { //when the "=" button is pressed
+        if (handleErrors()) return;
         displayed = displayed.filter(n => n !== ''); //removes all spaces stored in array due to .split()    
-        console.log(displayed);
         display.value = getOperations(displayed);
      }
     else if (button.value === regexNum || regexOp) { //if any number or operator is pressed (except "=")
@@ -24,11 +24,11 @@ function toDisplay(button) {  //adds the value of the button to the display
         displayed = display.value.split(regexOp); //makes an array of the display values,
                                                 //separating the numbers with the operators 
                                                 //& storing both (number & operator) in the array
-        console.log(displayed);
+        if (handleErrors()) return;
     }
 }
  
-let temp;
+let result;
 
 function getOperations(displayed) { //self note: i = 1 starts on operator (check try/catch if 1st button pressed is an operator)
     let numBeforeOp, numAfterOp, operator;
@@ -36,10 +36,21 @@ function getOperations(displayed) { //self note: i = 1 starts on operator (check
         numBeforeOp = Number(displayed[i - 1]);  //convert to number
         numAfterOp = Number(displayed[i + 1]);
         operator = displayed[i];
-        temp = operate(numBeforeOp, numAfterOp, operator); //store 1st operation result in 2nd operation, first operand
-        displayed[i + 1] = temp;
+        result = operate(numBeforeOp, numAfterOp, operator);
+        result = result.toFixed(3); //convert result to string with 3 decimals
+        displayed[i + 1] = result; //store 1st operation result in 2nd operation, first operand
     }
-    return temp;
+    return result;
+}
+
+function handleErrors() { //checks if '=' button (displayed===undefined) or if an operator (displayed[0]==='') were 1st clicked
+                          //Note: because of display.value.split(regexOp), if an operator its 1st clicked, displayed becomes
+                          //      displayed = ['', operator, '']
+    if ((displayed === undefined || displayed[0] === '')) {
+        console.log('Please clear and enter a valid operation');
+        return true; //theres been an error
+    }
+    else return false; //the operation is valid
 }
 
 function add(a, b) {
